6cc3e2f21c25cf31692cc2c40879fa913cf8851f
haripriya17 <haripriyamekala517@gmail.com>
2024-10-14 14:13:50 -0400

    added table reservation feature

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
-import Header from "./components/Header.js";
-import Nav from "./components/Nav.js";
-import Main from "./components/Main.js";
-import Footer from "./components/Footer.js";
-function App() {
+import { Route, Routes } from "react-router-dom";
+import pages from "./utils/pages";
+import Layout from "./components/layout/Layout";
+import Home from "./components/pages/Home";
+import Bookings from "./components/pages/Bookings";
+import ConfirmedBooking from "./components/pages/Bookings/ConfirmedBooking";
+import NotFound from "./components/pages/NotFound";
+import UnderConstruction from "./components/pages/UnderConstruction";
+
+const App = () => {
   return (
     <>
-      <Header />
-      <Nav />
-      <Main />
-      <Footer />
+      <Layout>
+        <Routes>
+          <Route path={pages.get("home").path} element={<Home />} />
+          <Route
+            path={pages.get("about").path}
+            element={<UnderConstruction />}
+          />
+          <Route
+            path={pages.get("menu").path}
+            element={<UnderConstruction />}
+          />
+          <Route path={pages.get("bookings").path} element={<Bookings />} />
+          <Route
+            path={pages.get("confirmedBooking").path}
+            element={<ConfirmedBooking />}
+          />
+          <Route
+            path={pages.get("orderOnline").path}
+            element={<UnderConstruction />}
+          />
+          <Route
+            path={pages.get("login").path}
+            element={<UnderConstruction />}
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
     </>
   );
-}
+};
 
 export default App;
